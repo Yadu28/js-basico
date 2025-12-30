be5732122b4f8f113aaeeea9c5d8f709e32d2335
Yadu28 <ortegaduvan11@gmail.com>
2025-12-30 10:35:20 -0500

    v1

---
+++ variables.js
@@ -0,0 +1,13 @@
+let videojuegos = 'Cuphead'
+console.log(videojuegos)
+
+// Lo NO permitido
+let c  = 'Cyberpunk'
+let cda = 'Call of duty'
+let pcVideojuego = 'Razer'
+let primerVideojuegoJugado = 'Mario Bros'
+
+//Lo permitido
+let primerVideojuegoDeDuvan = 'Bomberman'
+let urlDeUsuario = 'https://www.google.com'
+let idDeusuario = '123456789'
